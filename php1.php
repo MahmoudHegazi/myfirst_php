e6ef830ce3d7af6961467b1070880e9143497401
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2020-07-15 15:28:55 +0200

    Create php1.php

---
+++ php1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+class Goodbye {
+  const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
+  const hello = "hello world";
+}
+
+echo Goodbye::hello;
+?>
+
+</body>
+</html>
